167edcedb0bdb4203348c8c6c68ff789ecdf2b03
PaKas <KasymbekovPN@yandex.ru>
2020-05-19 22:49:31 +0300

    + add 'SkeletonClassSignatureSEHTest'

---
+++ src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/handler/clazz/classes/forSignature/TCForSignaturePublic.java
@@ -0,0 +1,7 @@
+package org.KasymbekovPN.Skeleton.custom.serialization.handler.clazz.classes.forSignature;
+
+import org.KasymbekovPN.Skeleton.lib.annotation.SkeletonClass;
+
+@SkeletonClass
+public class TCForSignaturePublic {
+}
