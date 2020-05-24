caab7b028653717bf6eeaceb805804d0f5ed766f
PaKas <KasymbekovPN@yandex.ru>
2020-05-24 17:33:46 +0300

    + add 'SkeletonExtendedTypeMemberSEHTest'

---
+++ src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/handler/member/classes/extended/TC0.java
@@ -0,0 +1,20 @@
+package org.KasymbekovPN.Skeleton.custom.serialization.handler.member.classes.extended;
+
+import org.KasymbekovPN.Skeleton.lib.annotation.SkeletonClass;
+import org.KasymbekovPN.Skeleton.lib.annotation.SkeletonMember;
+
+@SkeletonClass
+public class TC0 {
+
+    @SkeletonMember
+    public Integer publicProperty;
+
+    @SkeletonMember
+    protected Double protectedProperty;
+
+    @SkeletonMember
+    private Float privateProperty;
+
+    @SkeletonMember
+    static Long staticProperty;
+}
---
+++ src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/handler/member/classes/extended/TC1.java
@@ -0,0 +1,8 @@
+package org.KasymbekovPN.Skeleton.custom.serialization.handler.member.classes.extended;
+
+public class TC1 {
+    public Integer publicProperty;
+    protected Double protectedProperty;
+    private Float privateProperty;
+    static Long staticProperty;
+}
