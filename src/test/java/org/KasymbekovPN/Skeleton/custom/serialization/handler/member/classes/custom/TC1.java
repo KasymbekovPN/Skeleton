69e145ac8ad84500d718bbd05aa56fe710314b45
PaKas <KasymbekovPN@yandex.ru>
2020-05-24 16:47:37 +0300

    + 'SkeletonCustomMemberSEHTest'

---
+++ src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/handler/member/classes/custom/TC1.java
@@ -0,0 +1,8 @@
+package org.KasymbekovPN.Skeleton.custom.serialization.handler.member.classes.custom;
+
+public class TC1 {
+    public CustomClass1 publicProperty;
+    protected CustomClass2 protectedProperty;
+    private CustomClass3 privateProperty;
+    static CustomClass4 staticProperty;
+}
