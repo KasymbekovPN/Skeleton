bd699e5492e466c348663ef994314d3aac1c4c45
PaKas <KasymbekovPN@yandex.ru>
2020-07-27 22:39:10 +0300

    * fix

--- src/main/java/org/KasymbekovPN/Skeleton/lib/collector/process/SkeletonCollectorProcess.java
+++ src/main/java/org/KasymbekovPN/Skeleton/lib/collector/process/SkeletonCollectorProcess.java
@@ -12,23 +12,23 @@ public class SkeletonCollectorProcess implements CollectorProcess{
 
     private static final Logger log = LoggerFactory.getLogger(SkeletonCollectorProcess.class);
 
-    private final Map<Class<? extends Node>, CollectorProcessHandler> handlers = new HashMap<>();
+    private final Map<EntityItem, CollectorProcessHandler> handlers = new HashMap<>();
 
     public SkeletonCollectorProcess() {
     }
 
     @Override
     public void handle(Node node) {
-        Class<? extends Node> clazz = node.getClass();
-        if (handlers.containsKey(clazz)){
-            handlers.get(clazz).handle(node);
+        EntityItem ei = node.getEI();
+        if (handlers.containsKey(ei)){
+            handlers.get(ei).handle(node);
         } else {
-            log.error("The handler for {} doesn't exist", clazz.getCanonicalName());
+            log.error("The handler for {} doesn't exist", ei);
         }
     }
 
     @Override
     public void addHandler(EntityItem handlerId, CollectorProcessHandler collectorProcessHandler) {
-
+        handlers.put(handlerId, collectorProcessHandler);
     }
 }
--- src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/SkeletonSerializerGroupTest.java
+++ src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/SkeletonSerializerGroupTest.java
@@ -1,6 +1,13 @@
 package org.KasymbekovPN.Skeleton.custom.serialization;
 
+import org.KasymbekovPN.Skeleton.custom.collector.process.writing.handler.json.JsonArrayWCPH;
+import org.KasymbekovPN.Skeleton.custom.collector.process.writing.handler.json.JsonObjectWCPH;
+import org.KasymbekovPN.Skeleton.custom.collector.process.writing.handler.json.JsonPrimitiveWCPH;
 import org.KasymbekovPN.Skeleton.custom.collector.process.writing.handler.utils.Utils;
+import org.KasymbekovPN.Skeleton.custom.filter.string.IgnoreStringFilter;
+import org.KasymbekovPN.Skeleton.custom.format.offset.SkeletonOffset;
+import org.KasymbekovPN.Skeleton.custom.format.writing.json.formatter.*;
+import org.KasymbekovPN.Skeleton.custom.format.writing.json.handler.JsonWritingFormatterHandler;
 import org.KasymbekovPN.Skeleton.custom.serialization.classes.TC0;
 import org.KasymbekovPN.Skeleton.custom.serialization.classes.TC1;
 import org.KasymbekovPN.Skeleton.custom.serialization.clazz.handler.clazz.ClassAnnotationDataSEH;
@@ -10,6 +17,7 @@ import org.KasymbekovPN.Skeleton.custom.serialization.clazz.handler.member.Conta
 import org.KasymbekovPN.Skeleton.custom.serialization.clazz.handler.member.CustomMemberSEH;
 import org.KasymbekovPN.Skeleton.custom.serialization.clazz.handler.method.ToStringMethodSEH;
 import org.KasymbekovPN.Skeleton.custom.serialization.clazz.serializer.SkeletonSerializer;
+import org.KasymbekovPN.Skeleton.custom.serialization.group.handler.SkeletonSerializerGroupVisitor;
 import org.KasymbekovPN.Skeleton.custom.serialization.group.serializer.SerializerGroupEI;
 import org.KasymbekovPN.Skeleton.custom.serialization.group.serializer.SkeletonSerializerGroup;
 import org.KasymbekovPN.Skeleton.lib.annotation.handler.AnnotationChecker;
@@ -17,9 +25,14 @@ import org.KasymbekovPN.Skeleton.lib.annotation.handler.SkeletonAnnotationChecke
 import org.KasymbekovPN.Skeleton.lib.collector.Collector;
 import org.KasymbekovPN.Skeleton.lib.collector.handler.CollectorCheckingHandler;
 import org.KasymbekovPN.Skeleton.lib.collector.handler.SkeletonCollectorCheckingHandler;
+import org.KasymbekovPN.Skeleton.lib.collector.node.*;
+import org.KasymbekovPN.Skeleton.lib.collector.process.CollectorProcess;
 import org.KasymbekovPN.Skeleton.lib.collector.process.SkeletonCollectorProcess;
 import org.KasymbekovPN.Skeleton.lib.collector.process.checking.SkeletonCollectorCheckingProcess;
+import org.KasymbekovPN.Skeleton.lib.collector.process.writing.SkeletonCollectorWritingProcess;
+import org.KasymbekovPN.Skeleton.lib.collector.process.writing.SkeletonWCPH;
 import org.KasymbekovPN.Skeleton.lib.format.entity.EntityItem;
+import org.KasymbekovPN.Skeleton.lib.format.writing.handler.WritingFormatterHandler;
 import org.KasymbekovPN.Skeleton.lib.serialization.clazz.serializer.Serializer;
 import org.KasymbekovPN.Skeleton.lib.serialization.group.serializer.SerializerGroup;
 import org.KasymbekovPN.Skeleton.lib.utils.checking.TypeChecker;
@@ -29,15 +42,74 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @DisplayName("SkeletonSerializerGroup: Testing of:")
 public class SkeletonSerializerGroupTest {
 
-    @Test
-    void test() throws Exception {
+    private WritingFormatterHandler createWFH() throws Exception {
 
-        Collector collector = Utils.createCollector();
+        SkeletonOffset offset = new SkeletonOffset("    ");
+
+        WritingFormatterHandler wfh = new JsonWritingFormatterHandler.Builder(offset)
+                .addFormatter(ObjectNode.ei(), new JsonObjectWritingFormatter(offset))
+                .addFormatter(ArrayNode.ei(), new JsonArrayWritingFormatter(offset))
+                .addFormatter(BooleanNode.ei(), new JsonBooleanWritingFormatter(offset))
+                .addFormatter(CharacterNode.ei(), new JsonCharacterWritingFormatter(offset))
+                .addFormatter(NumberNode.ei(), new JsonNumberWritingFormatter(offset))
+                .addFormatter(StringNode.ei(), new JsonStringWritingFormatter(offset))
+                .build();
+
+        return wfh;
+    }
+
+    private CollectorProcess createProcess(WritingFormatterHandler wfh) throws Exception {
+
+
+        CollectorProcess process = new SkeletonCollectorWritingProcess();
+        new SkeletonWCPH(
+                new JsonArrayWCPH(),
+                wfh,
+                process,
+                ArrayNode.ei()
+        );
+        new SkeletonWCPH(
+                new JsonPrimitiveWCPH(),
+                wfh,
+                process,
+                BooleanNode.ei()
+        );
+        new SkeletonWCPH(
+                new JsonPrimitiveWCPH(),
+                wfh,
+                process,
+                CharacterNode.ei()
+        );
+        new SkeletonWCPH(
+                new JsonPrimitiveWCPH(),
+                wfh,
+                process,
+                NumberNode.ei()
+        );
+        new SkeletonWCPH(
+                new JsonObjectWCPH(new IgnoreStringFilter("annotation")),
+                wfh,
+                process,
+                ObjectNode.ei()
+        );
+        new SkeletonWCPH(
+                new JsonPrimitiveWCPH(),
+                wfh,
+                process,
+                StringNode.ei()
+        );
+
+        return process;
+    }
+
+    private Serializer createSerializer(Collector collector) throws Exception {
+
         AnnotationChecker sac = new SkeletonAnnotationChecker();
         CollectorCheckingHandler cch = new SkeletonCollectorCheckingHandler(SkeletonCollectorCheckingProcess.class);
         SkeletonCAC skeletonCAC = new SkeletonCAC(
@@ -56,6 +128,15 @@ public class SkeletonSerializerGroupTest {
                 .addMethodHandler(new ToStringMethodSEH(sac, cch))
                 .build();
 
+        return serializer;
+    }
+
+    @Test
+    void test() throws Exception {
+
+        Collector collector = Utils.createCollector();
+        Serializer serializer = createSerializer(collector);
+
         EntityItem sgKey = SerializerGroupEI.commonEI();
 
         SerializerGroup serializerGroup = new SkeletonSerializerGroup.Builder(new SkeletonCollectorProcess())
@@ -69,23 +150,18 @@ public class SkeletonSerializerGroupTest {
             add("int");
         }};
 
-//        CollectorWritingProcess process = Utils.createCollectorWritingProcess();
-        //<  !!! remake
-//        new JsonObjectWritingCollectorProcessHandler(process, ObjectNode.class, new IgnoredPropertyNameChecker("annotation"));
-//        new JsonArrayWritingCollectorProcessHandler(process, ArrayNode.class);
-//        new JsonBooleanWritingHandler(process, BooleanNode.class);
-//        new CharacterWritingHandler(process, CharacterNode.class);
-//        new NumberWritingHandler(process, NumberNode.class);
-//        new StringWritingHandler(process, StringNode.class);
+        WritingFormatterHandler wfh = createWFH();
+        CollectorProcess process = createProcess(wfh);
 
-//        SkeletonSerializerGroupVisitor visitor = new SkeletonSerializerGroupVisitor(
-//                new SkeletonCollectorCheckingHandler(SkeletonCollectorCheckingProcess.class),
-//                process,
-//                systemTypes
-//        );
-//        serializerGroup.accept(visitor);
-//
-//        Optional<String> mayBeData = visitor.getData();
-//        mayBeData.ifPresent(System.out::println);
+        SkeletonSerializerGroupVisitor visitor = new SkeletonSerializerGroupVisitor(
+                new SkeletonCollectorCheckingHandler(SkeletonCollectorCheckingProcess.class),
+                process,
+                wfh,
+                systemTypes
+        );
+        serializerGroup.accept(visitor);
+
+        Optional<String> mayBeData = visitor.getData();
+        mayBeData.ifPresent(System.out::println);
     }
 }
