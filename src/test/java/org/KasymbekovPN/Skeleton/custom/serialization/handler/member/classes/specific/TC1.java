653ee3c298d6644ae9e2f02981a3b7b7d0f6abea
PaKas <KasymbekovPN@yandex.ru>
2020-07-30 22:55:08 +0300

    * SpecificTypeMemberSEHTest is done

--- src/test/java/org/KasymbekovPN/Skeleton/custom/serialization/handler/member/classes/specific/TC1.java
+++
@@ -1,9 +0,0 @@
-package org.KasymbekovPN.Skeleton.custom.serialization.handler.member.classes.specific;
-
-//<
-//public class TC1 {
-//    private int privateProperty;
-//    protected boolean protectedProperty;
-//    public float publicProperty;
-//    static double staticProperty;
-//}
